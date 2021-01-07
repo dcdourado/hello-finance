60808ebd40e845b19a6055a1a1699af7391c6d6d
dcdourado <dcdourado@gmail.com>
2021-01-07 15:57:39 -0300

    user build test suite

---
+++ test/hello_finance/user_test.exs
@@ -0,0 +1,61 @@
+defmodule HelloFinance.UserTest do
+  use HelloFinance.DataCase
+
+  alias HelloFinance.{User, Repo}
+
+  describe "build/1" do
+    test "when all params are valid, returns a valid changeset" do
+      params = %{name: "Diogo", email: "dcdourado@gmail.com", password: "123456"}
+
+      result = User.build(params)
+
+      assert {:ok, %User{name: "Diogo", email: "dcdourado@gmail.com", password: "123456"}} =
+               result
+    end
+
+    test "when a required param is not inserted, returns an error" do
+      no_name_params = %{email: "dcdourado@gmail.com", password: "123456"}
+      {:error, no_name_result} = User.build(no_name_params)
+
+      assert %Ecto.Changeset{valid?: false} = no_name_result
+      assert errors_on(no_name_result) == %{name: ["can't be blank"]}
+
+      no_email_params = %{name: "Diogo", password: "123456"}
+      {:error, no_email_result} = User.build(no_email_params)
+
+      assert %Ecto.Changeset{valid?: false} = no_email_result
+      assert errors_on(no_email_result) == %{email: ["can't be blank"]}
+
+      no_password_params = %{name: "Diogo", email: "dcdourado@gmail.com"}
+      {:error, no_password_result} = User.build(no_password_params)
+
+      assert %Ecto.Changeset{valid?: false} = no_password_result
+      assert errors_on(no_password_result) == %{password: ["can't be blank"]}
+    end
+
+    test "when password do not attend min length (6), returns an error" do
+      params = %{name: "Diogo", email: "dcdourado@gmail.com", password: "12345"}
+      {:error, result} = User.build(params)
+
+      assert %Ecto.Changeset{valid?: false} = result
+      assert errors_on(result) == %{password: ["should be at least 6 character(s)"]}
+    end
+
+    test "when email has invalid format, returns an error" do
+      params = %{name: "Diogo", email: "dcdourado.com", password: "123456"}
+      {:error, result} = User.build(params)
+
+      assert %Ecto.Changeset{valid?: false} = result
+      assert errors_on(result) == %{email: ["has invalid format"]}
+    end
+
+    # test "when email is already used, returns an error" do
+    #   params = %{name: "Diogo", email: "dcdourado@gmail.com", password: "123456"}
+    #   {:ok, valid_user} = User.build(params)
+
+    #   Repo.insert(valid_user)
+
+    #   assert "a" == "b"
+    # end
+  end
+end
