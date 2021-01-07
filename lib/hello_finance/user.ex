7f9fd6da01bd58777baa663bcd21ffdfee5e9ce7
dcdourado <dcdourado@gmail.com>
2021-01-07 14:46:42 -0300

    User module

---
+++ lib/hello_finance/user.ex
@@ -0,0 +1,39 @@
+defmodule HelloFinance.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    timestamps()
+  end
+
+  @required_params [:name, :email, :password]
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  defp changeset(params), do: create_changeset(%__MODULE__{}, params)
+
+  defp create_changeset(module, params) do
+    module
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
+end
