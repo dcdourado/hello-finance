bdd05665bfd3aac723feb2614d129ec92aeaeaa6
dcdourado <dcdourado@gmail.com>
2021-01-07 11:19:26 -0300

    add_transfers_table

---
+++ priv/repo/migrations/20210107141635_add_transfer_table.exs
@@ -0,0 +1,12 @@
+defmodule HelloFinance.Repo.Migrations.AddTransferTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:transfers) do
+      add :value, :integer
+      add :currency, :string
+      add :sender_account_id, references(:accounts)
+      add :receiver_account_id, references(:accounts)
+    end
+  end
+end
