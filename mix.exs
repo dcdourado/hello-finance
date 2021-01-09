738bf1bbf9e8b81af97f30258a338e408fb33356
dcdourado <dcdourado@gmail.com>
2021-01-09 08:16:56 -0300

    ExchangeApi get_rate and add Tesla dep

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule HelloFinance.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:tesla, "~> 1.4.0"}
     ]
   end
 
