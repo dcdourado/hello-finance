01fe2025649a556a91fd6781e186d3ca326db451
dcdourado <dcdourado@gmail.com>
2021-01-10 19:30:05 -0300

    raw User.Account.Transfer.Create - account changesets error catch change

--- lib/hello_finance.ex
+++ lib/hello_finance.ex
@@ -1,8 +1,12 @@
 defmodule HelloFinance do
+  alias HelloFinance.Currency
   alias HelloFinance.User
   alias User.Account
 
   defdelegate create_user(params), to: User.Create, as: :call
 
   defdelegate create_account(params), to: Account.Create, as: :call
+  defdelegate get_account(params), to: Account.Get, as: :call
+
+  defdelegate exchange_currency(from_code, from_value, to_code), to: Currency.Exchange, as: :call
 end
--- lib/hello_finance/user/account.ex
+++ lib/hello_finance/user/account.ex
@@ -24,6 +24,8 @@ defmodule HelloFinance.User.Account do
 
   defp changeset(params), do: create_changeset(%__MODULE__{}, params)
 
+  def changeset(account, params), do: create_changeset(account, params)
+
   defp create_changeset(module, params) do
     module
     |> cast(params, @required_params)
@@ -32,22 +34,24 @@ defmodule HelloFinance.User.Account do
     |> validate_user()
   end
 
-  defp validate_money(%Changeset{valid?: false} = changeset), do: changeset
-
   # TODO: add_error separately for each case
-  defp validate_money(%Changeset{changes: %{currency: currency, balance: balance}} = changeset) do
+  defp validate_money(
+         %Changeset{valid?: true, changes: %{currency: currency, balance: balance}} = changeset
+       ) do
     case Currency.build(currency, balance) do
       {:ok, _currency} -> changeset
       _error -> changeset |> add_error(:currency, "invalid") |> add_error(:balance, "invalid")
     end
   end
 
-  defp validate_user(%Changeset{valid?: false} = changeset), do: changeset
+  defp validate_money(changeset), do: changeset
 
-  defp validate_user(%Changeset{changes: %{user_id: id}} = changeset) do
+  defp validate_user(%Changeset{valid?: true, changes: %{user_id: id}} = changeset) do
     case User.Get.call(id) do
       {:ok, _user} -> changeset
       _error -> add_error(changeset, :user_id, "not found")
     end
   end
+
+  defp validate_user(changeset), do: changeset
 end
