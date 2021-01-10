88386e86a9986c01d4047d18f87a6d981c49b367
dcdourado <dcdourado@gmail.com>
2021-01-10 10:14:27 -0300

    updated and done exchange strategy

--- lib/hello_finance/currency/exchange.ex
+++ lib/hello_finance/currency/exchange.ex
@@ -1,24 +1,33 @@
 defmodule HelloFinance.Currency.Exchange do
   alias HelloFinance.Currency
+  alias HelloFinance.ExchangeApi.Client
 
-  def call(from_code, from_value, to_code, to_value) do
-    build_currencies(from_code, from_value, to_code, to_value)
-  end
-
-  defp build_currencies(from_code, from_value, to_code, to_value) do
+  def call(from_code, from_value, to_code) do
     Currency.build(from_code, from_value)
-    |> put_currency(Currency.build(to_code, to_value))
+    |> get_exchange_rate(to_code)
+    |> parse_currency(to_code, from_value)
   end
 
-  defp put_currency({:ok, from_currency}, {:ok, to_currency}) do
-    %{from: from_currency, to: to_currency}
+  defp get_exchange_rate({:error, _message} = error, _to_code), do: error
+
+  defp get_exchange_rate({:ok, %{code: from_code, value: _value}}, to_code) do
+    to_code = parse_code_to_string(to_code)
+
+    case Client.get_rate(from_code, to_code) do
+      {:ok, %{"rates" => %{^to_code => to_rate}}} -> to_rate
+      error -> error
+    end
   end
 
-  defp put_currency({:error, [message: message]}, _to_currency) do
-    {:error, message: "from currency #{message}"}
+  defp parse_code_to_string(code) when is_binary(code), do: code
+
+  defp parse_code_to_string(code) when is_atom(code) do
+    Atom.to_string(code)
   end
 
-  defp put_currency(_from_currency, {:error, [message: message]}) do
-    {:error, message: "to currency #{message}"}
+  defp parse_currency({:error, _message} = error, _to_code, _from_value), do: error
+
+  defp parse_currency(rate, to_code, from_value) do
+    Currency.build(to_code, trunc(rate * from_value))
   end
 end
