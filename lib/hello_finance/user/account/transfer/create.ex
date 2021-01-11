240f785df1595c6b86148169cbc387d510d728c2
dcdourado <dcdourado@gmail.com>
2021-01-11 14:59:44 -0300

    User.Account.Transfer.Create: currency -> code

--- lib/hello_finance/user/account/transfer/create.ex
+++ lib/hello_finance/user/account/transfer/create.ex
@@ -15,7 +15,7 @@ defmodule HelloFinance.User.Account.Transfer.Create do
   defp create_transfer(
          {:ok,
           %Transfer{
-            currency: currency,
+            code: code,
             value: value,
             sender_account_id: sender_account_id,
             receiver_account_id: receiver_account_id
@@ -25,29 +25,29 @@ defmodule HelloFinance.User.Account.Transfer.Create do
     |> Multi.insert(:transfer, struct)
     |> Multi.update(
       :sender_account,
-      get_account_transfer_changeset(sender_account_id, currency, -value)
+      get_account_transfer_changeset(sender_account_id, code, -value)
     )
     |> Multi.update(
       :receiver_account,
-      get_account_transfer_changeset(receiver_account_id, currency, value)
+      get_account_transfer_changeset(receiver_account_id, code, value)
     )
     |> Repo.transaction()
   end
 
-  defp get_account_transfer_changeset(account_id, currency, value) do
-    {:ok, %{currency: account_currency, balance: account_balance} = account} =
+  defp get_account_transfer_changeset(account_id, code, value) do
+    {:ok, %{code: account_code, balance: account_balance} = account} =
       HelloFinance.get_account(account_id)
 
-    value = get_exchanged_value(value, currency, account_currency)
+    value = get_exchanged_value(value, code, account_code)
 
     Account.changeset(account, %{balance: account_balance + value})
   end
 
-  defp get_exchanged_value(value, from_currency, to_currency) when from_currency == to_currency,
+  defp get_exchanged_value(value, from_code, to_code) when from_code == to_code,
     do: value
 
-  defp get_exchanged_value(value, from_currency, to_currency) when from_currency != to_currency do
-    {:ok, money} = HelloFinance.exchange_currency(value, from_currency, to_currency)
+  defp get_exchanged_value(value, from_code, to_code) when from_code != to_code do
+    {:ok, money} = HelloFinance.exchange_code(value, from_code, to_code)
 
     %{value: value} = money
     value
