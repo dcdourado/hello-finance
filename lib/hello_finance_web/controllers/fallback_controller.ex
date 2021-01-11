9a5e68a0f6f0e661906c59e82a76378a8c236b5d
dcdourado <dcdourado@gmail.com>
2021-01-11 09:08:31 -0300

    401 view and render unauthorized calls

--- lib/hello_finance_web/controllers/fallback_controller.ex
+++ lib/hello_finance_web/controllers/fallback_controller.ex
@@ -1,6 +1,13 @@
 defmodule HelloFinanceWeb.FallbackController do
   use HelloFinanceWeb, :controller
 
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(HelloFinanceWeb.ErrorView)
+    |> render("401.json", result: "User unauthorized")
+  end
+
   def call(conn, {:error, result}) do
     conn
     |> put_status(:bad_request)
